dcb4d7cbdc01280881435d58317d55ef16b32356
myztillx <samuel.geist@gmail.com>
2024-10-21 17:33:51 +0000

    Refactor structure

--- mymeter/__init__.py
+++ mymeter/__init__.py
@@ -1,5 +1,8 @@
+"""Init file for pymymeter."""
+
+from .const import CostRead, UsageInterval, UsageRead, UsageType
 from .exceptions import DataException, InvalidAuth, TokenErrorException
-from .mymeter import CostRead, MyMeter, UsageInterval, UsageRead, UsageType
+from .mymeter import MyMeter
 
 __all__ = [
     "CostRead",
